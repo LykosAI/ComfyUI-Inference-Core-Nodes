13a6d33fc378b50d11badb5ca9f68ba28896b51a
Ionite <dev@ionite.io>
2024-02-21 15:16:00 -0500

    Add root module import

--- src/inference_core_nodes/__init__.py
+++ src/inference_core_nodes/__init__.py
@@ -1,1 +1,25 @@
+__all__ = ("__version__", "NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS")
+
 __version__ = "0.1.0"
+
+
+def _get_node_mappings():
+    import importlib
+    import pkgutil
+
+    node_class_mappings = {}
+    node_display_name_mappings = {}
+
+    for submodule_info in pkgutil.iter_modules(__path__):
+        # import
+        submodule = importlib.import_module(f"{__name__}.{submodule_info.name}")
+
+        node_class_mappings.update(getattr(submodule, "NODE_CLASS_MAPPINGS"))
+        node_display_name_mappings.update(
+            getattr(submodule, "NODE_DISPLAY_NAME_MAPPINGS")
+        )
+
+    return node_class_mappings, node_display_name_mappings
+
+
+NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS = _get_node_mappings()
