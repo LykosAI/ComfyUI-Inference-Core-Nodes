0e781a205e9801304cd48e984a3963e5056095cc
thecooltechguy <subby@galiboo.com>
2024-03-09 12:52:30 -0800

    fixed install.py script

--- install.py
+++ install.py
@@ -2,13 +2,14 @@ import sys
 import subprocess
 
 from pathlib import Path
+from typing import List
 
 PARENT_DIR = Path(__file__).parent
 
 
-def pip_install(package: str) -> None:
+def pip_install(args: List[str]) -> None:
     subprocess.check_call(
-        [sys.executable, "-m", "pip", "install", package], cwd=PARENT_DIR
+        [sys.executable, "-m", "pip", "install", ] + args, cwd=PARENT_DIR
     )
 
 
@@ -25,12 +26,12 @@ def main() -> None:
     cuda_version = try_get_cuda_version()
     if cuda_version is not None:
         if cuda_version.startswith("12."):
-            pip_install("-e .[cuda-12]")
+            pip_install(["-e", ".[cuda-12]"])
         else:
-            pip_install("-e .[cuda]")
+            pip_install(["-e", ".[cuda]"])
     else:
         # Default install
-        pip_install("-e .[cpu]")
+        pip_install(["-e", ".[cpu]"])
 
 
 if __name__ == "__main__":
